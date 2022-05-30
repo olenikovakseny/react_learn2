bd92caab92d95cfab5e310771078a7679a001ee2
olenikovakseny <olenikovakseny@gmail.com>
2022-05-30 15:04:59 +0300

    json users

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import './App.css';
 
 function App() {
   
-  
+  let [users, setUsers] = useState([]);
+
   useEffect(() => {
    
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(value => {
+      setUsers(value);
+    });
+
   }, []);
   
   return (
     <div>
+      {
+        users.map(value => {return <div key={value.id}>{value.id} - {value.name}</div>;})
+      }
 
     </div>
   );
