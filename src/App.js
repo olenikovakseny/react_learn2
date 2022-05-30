7ece3c3039e3a8c7376c4aa4bcb2f47f2530c073
olenikovakseny <olenikovakseny@gmail.com>
2022-05-30 19:59:14 +0300

    reducer ...state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import User from './components/user/User';
 import { getUsers } from './services/api';
 
+function reducer(state, action) {
+  switch (action.type){
+    case 'ADD_USERS':
+    return {...state, users: action.payload};
+    case 'ADD_USER':
+    return {...state, user: action.payload};
 
+    default:
+      return {...state};
+  }
+}
 
 export default function App() {
+  // let [users, setUsers] = useState([]);
+  // let [user, setUser] = useState(null);
   
-  let [users, setUsers] = useState([]);
-  let [user, setUser] = useState(null);
-  useEffect(() => {
-   
-    getUsers().then(value => setUsers([...value.data]));
 
-  }, []);
+  let [state, dispatch] = useReducer(reducer, {users: [], user: null});
+  let {users, user} = state;
+
+  useEffect(() => {
+     getUsers().then(value => dispatch({type:'ADD_USERS', payload: value.data})); }, []);
   
 const choose = (id) => {
   let choosenUser = users.find(value => value.id === id);
-  setUser(choosenUser);
+  dispatch({type: 'ADD_USER', payload: choosenUser});
+  // setUser(choosenUser);
 };
 
   return (
