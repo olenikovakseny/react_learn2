0bba54fb0683c832c9d4c5b9b7aca88eb9e7fdbe
olenikovakseny <olenikovakseny@gmail.com>
2022-05-30 10:37:09 +0300

    empty useEffect

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import './App.css';
 
 function App() {
   
-  let [x, setx] = useState(0);
+  
   useEffect(() => {
-    console.log('effect');
-  }, [x]);
+   
+  }, []);
   
   return (
     <div>
-     <button onClick={() => {
-       setx(++x);
-     }}>button</button>
 
     </div>
   );
